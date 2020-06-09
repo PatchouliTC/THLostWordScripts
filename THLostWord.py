e81e5c362e9b4cd1226324a22025574b1878c05a
PatchouliTC <1009609373@qq.com>
2020-06-10 00:08:14 +0800

    update main file

--- THLostWord.py
+++ THLostWord.py
@@ -3,6 +3,7 @@ import sys
 import logging
 from apscheduler.schedulers.background import BackgroundScheduler
 
+from six.moves.urllib.parse import parse_qsl, urlparse
 from airtest.utils.logger import set_root_logger_level
 from airtest.core.android.adb import ADB
 from airtest.core.api import connect_device
@@ -18,43 +19,38 @@ logger=init_logging(level=logging.INFO)
 if __name__ == "__main__":
 
     device,cycletime,plans=make_plan()
-
-
+    
     planmanager=pm()
+
     if not planmanager.set_plan(plans,cycletime):
         logger.info('None Plans,no more running')
         sys.exit(0)
-    deviceavilable=False
-    logger.info('Searching Avilable Android Devices...')
-    try:
-        devices = ADB().devices(state="device")
-        if len(devices) == 0:
-            raise Exception
-        for dev in devices:
-            if device==dev[0]:
-                logger.debug('Find Plan Device')
-                deviceavilable=True
-                break
-    except Exception as berr:
-        logger.info('No Useable Device,quit')
-        ADB().kill_server()
-        sys.exit(0)
-    
-    logger.info('Not find plan device in useable devices')
-    print(f'Avilable Devices---》》》')
-    for d in range(len(devices)):
-        print(f"{d+1}.{devices[d][0]}")
-    num=int(input('Enter Num to select:'))
-    try:
-        device=devices[num-1][0]
-    except:
-        logger.info('Error input,select default')
-        device=devices[0][0]
 
-    connect_device(f'android:///{device}')
+    if not device:
+        logger.info('Searching Avilable Local Android Devices...')
+        try:
+            devices = ADB().devices(state="device")
+            if len(devices) == 0:
+                raise Exception
+        except Exception as berr:
+            logger.info('No Useable Device,quit')
+            ADB().kill_server()
+            sys.exit(0)
+        print(f'Avilable Devices---》》》')
+        for d in range(len(devices)):
+            print(f"{d+1}.{devices[d][0]}")
+        num=int(input('Enter Num to select:'))
+        try:
+            device=devices[num-1][0]
+        except:
+            logger.info('Error input,select default')
+            device=devices[0][0]
 
-    planmanager.run_plans()
-
+        instance=connect_device(f'android:///{device}')
+    else:
+        instance=connect_device(device)
+    if instance:
+        planmanager.run_plans()
     logger.info('Plan_Finish')
     ADB().kill_server()
 
